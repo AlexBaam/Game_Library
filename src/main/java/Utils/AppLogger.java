ffe838edd62e06444e06979dad79c41a86cb89fa
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2025-05-31 16:42:18 +0300

    Add files via upload

---
+++ src/main/java/Utils/AppLogger.java
@@ -0,0 +1,33 @@
+package Utils;
+import java.io.IOException;
+import java.util.logging.*;
+
+public class AppLogger {
+
+    private static final Logger logger = Logger.getLogger("GameLibraryLogger");
+
+    static {
+        try {
+            LogManager.getLogManager().reset();
+            logger.setLevel(Level.ALL);
+
+            // Use colored console output
+            ConsoleHandler consoleHandler = new ConsoleHandler();
+            consoleHandler.setLevel(Level.ALL);
+            consoleHandler.setFormatter(new LoggerColor()); // Aici setez ca loggerul meu sa fie setat dupa propriile reguli
+            logger.addHandler(consoleHandler);
+
+            FileHandler fileHandler = new FileHandler("game_library.log", true);
+            fileHandler.setLevel(Level.FINE);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+
+        } catch (IOException e) {
+            logger.severe("Failed to initialize logger: " + e.getMessage());
+        }
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+}
